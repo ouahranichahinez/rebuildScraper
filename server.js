39d6b4a2d424f454e5e308085474a09298b1e42f
cchahinez <chahinez.work@gmail.com>
2022-07-15 11:50:22 +0100

    o

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import {getCompaniesPageUrl, getLinkedinUrls} from './bot.js'
+import timeout from 'connect-timeout'
 
 
 const app = express()
@@ -8,9 +9,9 @@ app.use(timeout('100s'))
 app.get('/', async (req, res)=>{
     //await bot ()
     const m= await getCompaniesPageUrl()
-    res.send("step 1 is finished")
+    //res.send("step 1 is finished")
     const n = await getLinkedinUrls(m)
-    //res.send("Your Script has finished... see you next time :) !")
+    res.send("Your Script has finished... see you next time :) !")
 })
 
 app.listen(PORT, () => {
