06806bee79fcb00325faa0ee0dd653b7e88c9fb9
cchahinez <chahinez.work@gmail.com>
2022-07-15 12:28:08 +0100

    s

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000
 app.get('/', async (req, res)=>{
     //await bot ()
     const m= await getCompaniesPageUrl()
-    //res.send("step 1 is finished")
+    res.send("step 1 is finished")
     const n = await getLinkedinUrls(m)
     res.send("Your Script has finished... see you next time :) !")
 })
